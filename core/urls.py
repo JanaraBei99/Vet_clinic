72af6e08a43ad1c8e715998838282400e7ff5226
Janara Beishebaeva <bjanarat.kstu.com>
2025-06-28 19:29:00 +0600

    fix static

--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 schema_view = get_schema_view(
@@ -44,3 +46,6 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('parser/', include('apps.parser.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
